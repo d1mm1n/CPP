4ae508274d6d68d995e65737365031224d4c3e31
d1mm1n <dimmining@naver.com>
2023-09-24 16:15:32 +0900

    소멸자 추가+ 코드 수정

--- 보고서#3/report_1.cpp
+++ 보고서#3/report_1.cpp
@@ -7,14 +7,22 @@ class Student {
 private:
 	string name;
 	double marks;
+
 public:
+	//생성자
 	Student() {
 		name = "Anonymous";
 		marks = 0;
+		cout << name << " is crying" << endl;
 	}
 
-	Student(string n, double m):name{n},marks{m}{}
+	Student(string n, double m):name{n},marks{m}{
+		cout << n << " is crying" << endl;
+	}
 	
+	//소멸자
+	~Student();
+
 	string Getname() {
 		return name;
 	}
@@ -23,12 +31,16 @@ public:
 	}
 
 };
+//소멸자
+Student ::~Student() {
+	cout << name << " is dying \n";
+}
 
-void print(Student s) {
-	cout << s.Getname() << " is crying"<<endl;
+//복사 생성자 호출을 방지 하기 위해 참조로 매개변수를 받습니다.
+//이렇게 해야지 Student객체의 복사본을 만들지 않으며, 생성자와 소멸자가 두 번씩 호출되지 않습니다.
+void print(Student &s) {
 	cout << "Name: "<<s.Getname()<<" ";
 	cout << "Marks: " << s.Getmarks() << " \n";
-	cout << s.Getname() << " is dying" << endl;
 }
 
 
@@ -36,9 +48,9 @@ int main() {
 	Student s1;
 	Student s2("Gildong", 4.5);
 
-
 	print(s1);
 	print(s2);
-
+	
+	return 0;
 
 }
